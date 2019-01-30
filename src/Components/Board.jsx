b5fc113145de959173309781245700e81db9153a
presenter <presenter@S34613.sogeti.com>
2019-01-30 16:26:51 -0500

    add tictactoe functionality

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -3,13 +3,62 @@ import Square from './Square';
 import '../index.css';
 
 class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      gameOver: false
+    };
+  }
+
+  handleClick(i) {
+    const newSquares = this.state.squares.slice();
+    newSquares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: newSquares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  renderSquare(i) {
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+   calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+ 
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
     }
+    return null;
+  }
   
     render() {
-      const status = 'Next player: X';
-  
+      // const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      const winner = this.calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
